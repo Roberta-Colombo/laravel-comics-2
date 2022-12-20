e5f7a680e3435a50d055b1043ec2c0af9229d03e
Roberta-Colombo <roberta.colombo89@hotmail.it>
2022-12-20 18:09:22 +0100

    Created model, controller, route and CRUD ops

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Guest\ComicController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('home');
+
+//route per controller e le resources create:
+Route::resource('comics', ComicController::class);
